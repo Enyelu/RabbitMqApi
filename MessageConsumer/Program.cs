a0ec06e395a7045242446146044754c0121172c8
Chukwuebuka Enyelu <cenyel@axxess.com>
2023-02-09 00:49:09 +0100

    configured message consumer

--- MessageConsumer/Program.cs
+++ MessageConsumer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "slim",
+    VirtualHost = "/"
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.QueueDeclare("user", true, true);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"A new user has been created {message}");
+};
+
+channel.BasicConsume("user", true,consumer);
