4f52129fa56583aab1865b7df09bf500e90d68d2
Chukwuebuka Enyelu <cenyel@axxess.com>
2023-02-08 16:24:49 +0100

    add rabbitMq publish service

---
+++ RabitMqApi/Services/Implementations/MessagePublisher.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RabbitMQ.Client;
+using RabitMqApi.Services.Interfaces;
+
+namespace RabitMqApi.Services.Implementations
+{
+    public class MessagePublisher : IMessagePublisher
+    {
+        private readonly IConfiguration _configuration;
+        public MessagePublisher(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void Publish<T>(T message)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMq:HostName"],
+                UserName = _configuration["RabbitMq:UserName"],
+                Password = _configuration["RabbitMq:Password"],
+                VirtualHost = "/"
+            };
+
+            var connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("user", true, true);
+
+            var messageJson = JsonSerializer.Serialize(message);
+            var messageToSend = Encoding.UTF8.GetBytes(messageJson);
+
+            channel.BasicPublish("", "user", body: messageToSend);
+        }
+    }
+}
