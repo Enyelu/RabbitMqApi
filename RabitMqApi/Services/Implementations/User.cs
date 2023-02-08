168038546aafb3bc5449fbb2d22562e398ef9bd2
Chukwuebuka Enyelu <cenyel@axxess.com>
2023-02-08 17:06:04 +0100

    add user service and implement user controller

---
+++ RabitMqApi/Services/Implementations/User.cs
@@ -0,0 +1,31 @@
+﻿using RabitMqApi.Data;
+using RabitMqApi.Services.Interfaces;
+using UserDto = RabitMqApi.Models.Dto;
+using NewUser = RabitMqApi.Models.Entities;
+
+namespace RabitMqApi.Services.Implementations
+{
+    public class User : IUser
+    {
+        private readonly IMessagePublisher _messagePublisher;
+        public User(IMessagePublisher messagePublisher)
+        {
+                _messagePublisher = messagePublisher;
+        }
+        public NewUser.User AddUser(UserDto.User user)
+        {
+            var newUser =  new NewUser.User
+            {
+                FirtName = user.FirtName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                Email = user.Email,
+            };
+
+            Storage.Users.Add(newUser);
+            _messagePublisher.Publish(newUser);
+
+            return newUser;
+        }
+    }
+}
